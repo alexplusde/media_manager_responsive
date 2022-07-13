0a7bffecd1775061984ec3d5a20abaad90791e3f
Alexander Walther <aw@alexplus.de>
2022-07-13 17:53:58 +0200

    settings page for cache_warmup

--- boot.php
+++ boot.php
@@ -14,3 +14,15 @@ if (\rex_addon::get('yform') && \rex_addon::get('yform')->isAvailable()) {
 // rex_extension::register('MEDIA_FORM_EDIT', array('rex_media_plus', 'mediapool_edit_svg_viewbox'));
 rex_extension::register('MEDIA_UPDATED', array('rex_media_plus', 'mediapool_updated_svg_viewbox'));
 // rex_extension::register('MEDIA_FORM_ADD', array('rex_media_plus', 'mediapool_add_svg_viewbox'));
+
+if(\rex_addon::get('cache_warmup') && \rex_addon::get('cache_warmup')->isAvailable()) {
+    if(rex_config::get('media_manager_responsive', 'cache_warmup') == "enhanced") {
+        rex_extension::register('PACKAGES_INCLUDED', function () {   
+            rex_addon::get('cache_warmup')->setConfig('chunkSizePages', 1);
+        });
+        rex_extension::register('CACHE_WARMUP_IMAGES', function (rex_extension_point $ep) {
+            // $images = $ep->getSubject();
+            return [];
+        });
+    }
+}
---
+++ pages/media_manager.media_manager_responsive.settings.php
@@ -0,0 +1,57 @@
+<?php
+
+$addon = rex_addon::get('media_manager_responsive');
+
+$form = rex_config_form::factory($addon->name);
+
+$field = $form->addSelectField('cache_warmup');
+$field->setLabel($this->i18n('media_manager_responsive_cache_warmup_label'));
+$field->setNotice($this->i18n('media_manager_responsive_cache_warmup_notice'));
+$select = $field->getSelect();
+$select->setSize(1);
+$select->addOption($this->i18n('media_manager_responsive_cache_warmup_original'), 'original');
+$select->addOption($this->i18n('media_manager_responsive_cache_warmup_enhanced'), 'enhanced');
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'edit', false);
+$fragment->setVar('title', $this->i18n('media_manager_responsive_settings'), false);
+$fragment->setVar('body', $form->get(), false);
+
+?>
+<div class="row">
+    <div class="col-lg-8">
+        <?= $fragment->parse('core/page/section.php') ?>
+    </div>
+    <div class="col-lg-4">
+        <?php
+
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=media_manager_responsive"><img src="'.rex_url::addonAssets('media_manager_responsive', 'jetzt-spenden.svg').'" style="width: 100% max-width: 400px;"></a>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'info', false);
+$fragment->setVar('title', $this->i18n('media_manager_responsive_donate'), false);
+$fragment->setVar('body', '<p>' . $this->i18n('media_manager_responsive_info_donate') . '</p>' . $anchor, false);
+echo !rex_config::get("alexplusde", "donated") ? $fragment->parse('core/page/section.php') : "";
+
+if (rex_addon::get('speed_up')->isAvailable()) {
+    $anchor = '<a target="_blank" href="https://github.com/alexplusde/speed_up/">'.$this->i18n('media_manager_responsive_info_media_manager_responsive_install').'</a>';
+    $fragment = new rex_fragment();
+    $fragment->setVar('class', 'info', false);
+    $fragment->setVar('title', $this->i18n('media_manager_responsive_info_speed_up_title'), false);
+    $fragment->setVar('body', '<p>' . $this->i18n('media_manager_responsive_info_speed_up_inactive') . '</p>' . $anchor, false);
+    echo $fragment->parse('core/page/section.php');
+}
+
+$package = rex_install_packages::getUpdatePackages();
+if (isset($packages['media_manager_responsive'])) {
+    $current_version = rex_addon::get('media_manager_responsive')->getProperty('version');
+    if (isset($package['files'])) {
+        $latest_version = array_pop($updates)['version'];
+    }
+    if (rex_version::compare($latest_version, $current_version, ">")) {
+        echo rex_view::info($this->i18n('media_manager_responsive_update_available') . " " .$latest_version);
+    };
+};
+        ?>
+    </div>
+</div>
