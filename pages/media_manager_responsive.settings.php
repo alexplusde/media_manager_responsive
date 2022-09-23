fb411170ed511861b1e2d3ed207556f8a445412e
Alexander Walther <aw@alexplus.de>
2022-09-23 19:56:49 +0200

    wording beauftragung

--- pages/media_manager_responsive.settings.php
+++ pages/media_manager_responsive.settings.php
@@ -6,7 +6,7 @@
     <div class="col-lg-4">
         <?php
 
-$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=media_manager_responsive"><img src="'.rex_url::addonAssets('media_manager_responsive', 'jetzt-spenden.svg').'" style="width: 100% max-width: 400px;"></a>';
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=media_manager_responsive"><img src="'.rex_url::addonAssets('media_manager_responsive', 'jetzt-beauftragen.svg').'" style="width: 100% max-width: 400px;"></a>';
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'info', false);
