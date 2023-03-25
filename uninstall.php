186010edeeeba9302dfbccc718bf713b4a409e1f
Alexander Walther <github.com@alexplus.de>
2023-03-25 09:31:47 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 
 // Alle Medientypen bei Uninstall exportieren. Nur zu Test- und Entwicklungszwecken
-
+/*
 $mediaTypes = rex_sql::factory()->setDebug(0)->getArray("SELECT * FROM rex_media_manager_type WHERE `name` LIKE '%media_plus.%'");
 
 rex_file::put(rex_path::addon("media_plus", "install/rex_media_manager_type.json"), json_encode($mediaTypes));
@@ -13,3 +13,4 @@ foreach ($mediaTypes as $mediaType) {
 $mediaEffects = rex_sql::factory()->setDebug(0)->getArray("SELECT E.effect, E.parameters, E.priority, T.name FROM (SELECT * FROM rex_media_manager_type_effect) E LEFT JOIN (SELECT `id`, `name` FROM rex_media_manager_type) T ON T.id = E.type_id WHERE FIND_IN_SET(`E`.`type_id`, :type_ids)", [":type_ids" => implode(",", $type_ids)]);
 
 rex_file::put(rex_path::addon("media_plus", "install/rex_media_manager_type_effects.json"), json_encode($mediaEffects));
+*/
