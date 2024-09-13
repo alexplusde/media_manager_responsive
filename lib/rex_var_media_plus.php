3fffb58ef2c0edb1f865eeedaffeec8c9eb95d45
Alexander Walther <mail@alexplus.de>
2024-09-14 00:13:42 +0200

    Update rex_var_media_plus.php

--- lib/rex_var_media_plus.php
+++ lib/rex_var_media_plus.php
@@ -12,27 +12,27 @@ class rex_var_media_plus extends rex_var
     protected function getOutput()
     {
         if ($this->hasArg('file') && $this->getArg('output') !== "") {
-            $media_plus = Media::get($this->getArg('file'));
+            $media = Media::get($this->getArg('file'));
             $profile = $this->getArg('profile') ?? '';
             $group = $this->getArg('group') ?? '';
-            if ($media_plus instanceof Media) {
+            if ($media instanceof Media) {
                 switch ($this->getArg('output')) {
                     case 'img':
-                        return self::quote($media_plus->getImg($profile));
+                        return self::quote($media->getImg($profile));
                     case 'img-src':
-                        return self::quote($media_plus->getImg($profile));
+                        return self::quote($media->getImg($profile));
                     case 'picture':
-                        return self::quote($media_plus->getPicture($group));
+                        return self::quote($media->getPicture($group));
                     case 'img-base64':
-                        return self::quote($media_plus->getImgBase64(false));
+                        return self::quote($media->getImgBase64(false));
                     case 'data-base64':
-                        return self::quote($media_plus->getImgBase64(true));
+                        return self::quote($media->getImgBase64(true));
                     case 'svg':
-                        return self::quote($media_plus->getSvg());
+                        return self::quote($media->getSvg());
                     case 'background':
-                        return self::quote($media_plus->getBackgroundStyles($group, ''));
+                        return self::quote($media->getBackgroundStyles($group, ''));
                     default:
-                        return self::quote($media_plus->getImg($profile));
+                        return self::quote($media->getImg($profile));
                 }
             }
         }
